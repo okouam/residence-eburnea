03efc68d9c9193fa8476cf151b688392b26edb3b
Olivier Kouame <olivier.kouame@salesforce.com>
2017-05-13 16:49:08 +0100

    All

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-	
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
@@ -64,5 +64,17 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
+
+  config.action_mailer.delivery_method = :smtp
+
+	config.action_mailer.smtp_settings = {
+	 :address => "smtp.gmail.com",
+	 :port => "587",
+	 :domain => "residence-eburnea.com",
+	 :user_name => "info@residence-eburnea.com",
+	 :password => "rBc}3K72962q",
+	 :authentication => "plain",
+	 :enable_starttls_auto => true
+ }
 
 end
